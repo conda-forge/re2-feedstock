f9bd54b77c46c2621dd52bf82441cbbb36d9f02a
Wes McKinney <wesm+git@apache.org>
2018-09-27 14:18:57 -0400

    Build both static and shared libraries on Unix and Windows platforms

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -18,3 +18,17 @@ if errorlevel 1 exit 1
 
 nmake install
 if errorlevel 1 exit 1
+
+@rem Build static libraries, install as re2_static.lib
+cmake -DBUILD_SHARED_LIBS=OFF ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -G "NMake Makefiles" ^
+      %SRC_DIR%
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+copy Release\re2.lib %LIBRARY_DIR%\re2_static.lib
+if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,11 @@ set -x
 builddir="${SRC_DIR}/cmake-build"
 mkdir -p ${builddir}
 cd ${builddir}
+
+export CXXFLAGS="-fPIC ${CXXFLAGS}"
+export CFLAGS="-fPIC ${CFLAGS}"
+
+# Build shared libraries
 cmake -DBUILD_SHARED_LIBS=ON \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
@@ -12,4 +17,13 @@ cmake -DBUILD_SHARED_LIBS=ON \
       "${SRC_DIR}"
 make -j "${CPU_COUNT}"
 ctest -E "dfa|exhaustive|random"
+make -j "${CPU_COUNT}" install
+
+# Build static libraries
+cmake -DBUILD_SHARED_LIBS=OFF \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}" \
+      "${SRC_DIR}"
+make -j "${CPU_COUNT}"
 make -j "${CPU_COUNT}" install
