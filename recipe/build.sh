ea279b9b369be77b1cc6db861525713114644572
Wes McKinney <wesm+git@apache.org>
2018-10-02 09:46:10 -0400

    Build using Makefile on Linux, skip on Windows vc<14

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,28 +2,8 @@
 set -e
 set -x
 
-builddir="${SRC_DIR}/cmake-build"
-mkdir -p ${builddir}
-cd ${builddir}
-
 export CXXFLAGS="-fPIC ${CXXFLAGS}"
 export CFLAGS="-fPIC ${CFLAGS}"
 
 # Build shared libraries
-cmake -DBUILD_SHARED_LIBS=ON \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-      -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}" \
-      "${SRC_DIR}"
-make -j "${CPU_COUNT}"
-ctest -E "dfa|exhaustive|random"
-make -j "${CPU_COUNT}" install
-
-# Build static libraries
-cmake -DBUILD_SHARED_LIBS=OFF \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-      -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}" \
-      "${SRC_DIR}"
-make -j "${CPU_COUNT}"
-make -j "${CPU_COUNT}" install
+make -j "${CPU_COUNT}" prefix=${PREFIX} install
