1c0f2082347ccb212eada7c801c0b007f8287ce4
Uwe L. Korn <uwe.korn@quantco.com>
2020-07-09 13:44:20 +0200

    CFEP-18: Remove static libraries from main build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -24,17 +24,3 @@ if errorlevel 1 exit 1
 
 nmake install
 if errorlevel 1 exit 1
-
-@rem Build static libraries, install as re2_static.lib
-cmake -DBUILD_SHARED_LIBS=OFF ^
-      -DCMAKE_BUILD_TYPE=Release ^
-      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -G "NMake Makefiles" ^
-      %SRC_DIR%
-if errorlevel 1 exit 1
-
-nmake
-if errorlevel 1 exit 1
-
-copy re2.lib %LIBRARY_LIB%\re2_static.lib
-if errorlevel 1 exit 1
