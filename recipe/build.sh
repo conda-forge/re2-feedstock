a459f4f8b7c85d83ebb98ad8dfb4bb13e1e9d998
Uwe L. Korn <uwe.korn@quantco.com>
2020-09-10 11:25:06 +0200

    Add cmake-based build to produce a CMake config

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-set -e
-set -x
+set -ex
 
-export CXXFLAGS="-fPIC ${CXXFLAGS}"
-export CFLAGS="-fPIC ${CFLAGS}"
-
-# Build shared libraries
+mkdir build-cmake
+pushd build-cmake
+cmake ${CMAKE_ARGS} -GNinja \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
+  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DENABLE_TESTING=OFF \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=ON \
+  ..
+ninja install
+popd
+# Also do this installation to get .pc files. This duplicates the compilation but gets us all necessary components without patching.
 make -j "${CPU_COUNT}" prefix=${PREFIX} shared-install
