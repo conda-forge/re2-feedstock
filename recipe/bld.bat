12ea30bc56d23a728a0b1c0ad7a6316ae41d54a8
H. Vetinari <h.vetinari@gmx.com>
2023-06-01 09:56:09 +1100

    clean up build scripts; normalize indentation

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,22 +6,20 @@ set "CXXFLAGS= -MD"
 
 mkdir -p cmake-build
 cd cmake-build
-set "VS_VERSION=14.0"
-set "VS_MAJOR=14"
-set "VS_YEAR=2015"
-cmake -DBUILD_SHARED_LIBS=ON ^
-      -DCMAKE_CXX_STANDARD=17 ^
-      -DCMAKE_BUILD_TYPE=Release ^
-      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -G "NMake Makefiles" ^
-      %SRC_DIR%
-if errorlevel 1 exit 1
+
+cmake -G "NMake Makefiles" ^
+    -DBUILD_SHARED_LIBS=ON ^
+    -DCMAKE_CXX_STANDARD=17 ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    ..
+if %ERRORLEVEL% neq 0 exit 1
 
 nmake
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
 
 ctest -E "dfa|exhaustive|random"
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
 
 nmake install
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -6,17 +6,20 @@ if [[ "${target_platform}" == osx-* ]]; then
 fi
 
 mkdir build-cmake
-pushd build-cmake
-cmake ${CMAKE_ARGS} -GNinja \
-  -DCMAKE_CXX_STANDARD=17 \
-  -DCMAKE_PREFIX_PATH=$PREFIX \
-  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-  -DCMAKE_INSTALL_LIBDIR=lib \
-  -DRE2_BUILD_TESTING=OFF \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DBUILD_SHARED_LIBS=ON \
-  ..
+cd build-cmake
+
+cmake -GNinja \
+    ${CMAKE_ARGS} \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    -DRE2_BUILD_TESTING=OFF \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    ..
+
 ninja install
-popd
+cd ..
 # Also do this installation to get .pc files. This duplicates the compilation but gets us all necessary components without patching.
 make -j "${CPU_COUNT}" prefix=${PREFIX} shared-install
