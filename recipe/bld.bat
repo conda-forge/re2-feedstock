8dab91f7a041790b4f7158a795e7cc8b2dbc09b9
H. Vetinari <h.vetinari@gmx.com>
2023-06-01 09:57:23 +1100

    don't rebuild twice on unix; use cmake+ninja exclusively

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,7 +7,7 @@ set "CXXFLAGS= -MD"
 mkdir -p cmake-build
 cd cmake-build
 
-cmake -G "NMake Makefiles" ^
+cmake -G Ninja ^
     -DBUILD_SHARED_LIBS=ON ^
     -DCMAKE_CXX_STANDARD=17 ^
     -DCMAKE_BUILD_TYPE=Release ^
@@ -15,11 +15,11 @@ cmake -G "NMake Makefiles" ^
     ..
 if %ERRORLEVEL% neq 0 exit 1
 
-nmake
+cmake --build .
 if %ERRORLEVEL% neq 0 exit 1
 
 ctest -E "dfa|exhaustive|random"
 if %ERRORLEVEL% neq 0 exit 1
 
-nmake install
+cmake --install .
 if %ERRORLEVEL% neq 0 exit 1
